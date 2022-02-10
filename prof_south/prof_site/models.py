a89a129a268f4d595582bd6f2a050ff9ff2fec1d
Юрий <senator23@mail.ru>
2022-02-10 12:36:16 +0300

    Add admin, models

--- prof_south/prof_site/models.py
+++ prof_south/prof_site/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+class Survey(models.Model):
+    surname_name = models.CharField(max_length=200)
+    question_date = models.DateTimeField('date published')
+    e_mail = models.EmailField()
+    phone_num = models.IntegerField()
+    question_text = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Сhairman(models.Model):
+    full_name = models.CharField(max_length=200)
+    position = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phones_num = models.IntegerField()
+    e_mail = models.EmailField()
+    chairman_photo = models.ImageField(default=1)
+
+    def __str__(self):
+        return self.name
+
+
+class Document(models.Model):
+    date_append = models.DateTimeField('date published')
+    document = models.FileField()
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=200)
+    position = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phones_num = models.IntegerField()
+    e_mail = models.EmailField()
+    employee_photo = models.ImageField(default=1)
+
+    def __str__(self):
+        return self.name
+
+
+class News(models.Model):
+    news_date = models.DateTimeField('date published')
+    news_text = models.TextField()
+    news_photo = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
+
+class ShortNewsOnMainPage(models.Model):
+    short_news_text = models.TextField()
+    short_news_photo = models.ImageField()
+
+    def __str__(self):
+        return self.name
